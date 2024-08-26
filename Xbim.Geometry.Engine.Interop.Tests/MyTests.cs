a35492af1a6781001e87844c156ba682e5c3de8c
Renato Silveira (LT) <RenatoS@leapthought.co.nz>
2024-08-26 17:51:17 -0300

    fix crashes and create tests

---
+++ Xbim.Geometry.Engine.Interop.Tests/MyTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using FluentAssertions;
+using Xbim.Ifc;
+using Xbim.ModelGeometry.Scene;
+
+
+namespace Xbim.Geometry.Engine.Interop.Tests
+{
+    [TestClass]
+    public class MyTests
+    {
+        [TestMethod]
+        public void Ifc4_MyTest_ARCHI_COMBINED()
+        {
+            using (var m = IfcStore.Open(@"C:\Dev\LT\IFC_Issues\ARCHI COMBINED.ifc"))
+            {
+                m.Instances.Count.Should().Be(150991);
+
+                var context = new Xbim3DModelContext(m);
+                context.CreateContext();
+
+                var numShapes = context.ShapeGeometries().ToArray().Length;
+                numShapes.Should().NotBe(0);
+
+                m.SaveAs(@"C:\Dev\LT\IFC_Issues\Tests\ARCHI_COMBINED_TEST.ifc");
+
+            }
+        }
+
+        [TestMethod]
+        public void Ifc4_MyTest_Architectural()
+        {
+            using (var m = IfcStore.Open(@"C:\Dev\LT\IFC_Issues\Architectural model (1).ifc"))
+            {
+                m.Instances.Count.Should().Be(167626);
+
+                var context = new Xbim3DModelContext(m);
+                context.CreateContext();
+
+                var numShapes = context.ShapeGeometries().ToArray().Length;
+                numShapes.Should().NotBe(0);
+
+                m.SaveAs(@"C:\Dev\LT\IFC_Issues\Tests\Architectural model (1)_TEST.ifc");
+            }
+        }
+
+        [TestMethod]
+        public void Ifc2x3_MyTest_1_Gridline()
+        {
+            using (var m = IfcStore.Open(@"C:\Dev\LT\IFC_Issues\1_Gridline test model.ifc"))
+            {
+                m.Instances.Count.Should().Be(28315);
+
+                var context = new Xbim3DModelContext(m);
+                context.CreateContext();
+                
+                var numShapes = context.ShapeGeometries().ToArray().Length;
+                numShapes.Should().NotBe(0);
+
+                m.SaveAs(@"C:\Dev\LT\IFC_Issues\Tests\1_Gridline test model_TEST.ifc");
+
+            }
+        }
+
+        [TestMethod]
+        public void Ifc2x3_MyTest_2_Gridline()
+        {
+            using (var m = IfcStore.Open(@"C:\Dev\LT\IFC_Issues\2_Gridline test model.ifc"))
+            {
+                m.Instances.Count.Should().Be(28316);
+
+                var context = new Xbim3DModelContext(m);
+                context.CreateContext();
+
+                var numShapes = context.ShapeGeometries().ToArray().Length;
+                numShapes.Should().NotBe(0);
+
+                m.SaveAs(@"C:\Dev\LT\IFC_Issues\Tests\2_Gridline test model_TEST.ifc");
+            }
+        }
+
+        [TestMethod]
+        public void Ifc2x3_MyTest_3_Gridline()
+        {
+            using (var m = IfcStore.Open(@"C:\Dev\LT\IFC_Issues\3_Gridline test model.ifc"))
+            {
+                m.Instances.Count.Should().Be(28315);
+
+                var context = new Xbim3DModelContext(m);
+                context.CreateContext();
+
+                var numShapes = context.ShapeGeometries().ToArray().Length;
+                numShapes.Should().NotBe(0);
+
+                m.SaveAs(@"C:\Dev\LT\IFC_Issues\Tests\3_Gridline test model_TEST.ifc");
+            }
+        }
+    }
+}
